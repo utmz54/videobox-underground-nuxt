5dcea35028d04f2bc2d5d9f4255bb2e664e37f33
Your Name <you@example.com>
2019-01-17 00:33:30 +0900

    ら抜き言葉

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">なにこれ</h1>
-          <p>個人的に好きなものを節操なく羅列していく為のページです。</p>
-          <p>今のところ見れるコンテンツは、バーチャルYoutuber<a href="https://www.youtube.com/channel/UCctWfjgLUh1yt-Wdz6W4yFQ" target="_blank">ミソシタ</a>の
-            <nuxt-link to="/miso/miso-karuta">ミソカルタ</nuxt-link>しかございません。ご容赦願います。
+          <p>個人的に好きなものを、節操なく羅列していく為のページです。</p>
+          <p>今のところ見られるコンテンツは、バーチャルYoutuber<a href="https://www.youtube.com/channel/UCctWfjgLUh1yt-Wdz6W4yFQ" target="_blank">ミソシタ</a>の
+            <nuxt-link to="/miso/miso-karuta">ミソカルタ</nuxt-link>しかございません。
           </p>
           <p><a href="https://twitter.com/KUMANOPUXTU" target="_blank">管理者連絡先</a></p>
         </div>
