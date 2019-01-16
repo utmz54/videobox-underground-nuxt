5720465b8c1cb9902e6e391a01c207711a5e14b8
Your Name <you@example.com>
2019-01-17 00:21:32 +0900

    連絡先の追加

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,10 @@
         <div class="container">
           <h1 class="title">なにこれ</h1>
           <p>個人的に好きなものを節操なく羅列していく為のページです。</p>
-          <p>今のところ見れるコンテンツは、バーチャルYoutuber<a href="https://www.youtube.com/channel/UCctWfjgLUh1yt-Wdz6W4yFQ">ミソシタ</a>の
-            <nuxt-link to="/miso/miso-karuta">ミソカルタ</nuxt-link>しかありません。
+          <p>今のところ見れるコンテンツは、バーチャルYoutuber<a href="https://www.youtube.com/channel/UCctWfjgLUh1yt-Wdz6W4yFQ" target="_blank">ミソシタ</a>の
+            <nuxt-link to="/miso/miso-karuta">ミソカルタ</nuxt-link>しかございません。ご容赦願います。
           </p>
+          <p><a href="https://twitter.com/KUMANOPUXTU" target="_blank">管理者連絡先</a></p>
         </div>
       </div>
     </section>
