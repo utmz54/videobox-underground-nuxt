2224c3959067693ba95e3e2a3d8f617bc53028ed
Your Name <you@example.com>
2019-01-20 23:10:10 +0900

    css調整

--- components/head.vue
+++ components/head.vue
@@ -3,8 +3,22 @@
     <div class="container">
       <div class="navbar-brand is-marginless">
         <nuxt-link class="navbar-item" to="/">地下二階</nuxt-link>
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          v-bind:class="{ 'is-active': menuActive }"
+          v-on:click="menuToggle()"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div id="navbarBasicExample" class="navbar-menu is-active">
+
+      <div id="navbarBasicExample" class="navbar-menu" v-bind:class="{ 'is-active': menuActive }">
         <div class="navbar-start">
           <nuxt-link class="navbar-item" to="/miso/miso-karuta">ミソカルタ</nuxt-link>
         </div>
@@ -12,5 +26,18 @@
     </div>
   </nav>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      menuActive: false
+    };
+  },
+  methods: {
+    menuToggle() {
+      this.menuActive = !this.menuActive;
+    }
+  }
+};
+</script>
 
