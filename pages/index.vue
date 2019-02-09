f44c7ffa6e4f56c183edc13ac05d4ddfa2e6f9b4
Your Name <you@example.com>
2019-02-10 03:15:38 +0900

    top

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,44 @@
 <template>
-  <section class="section">
-    <section class="section">
-      <div class="columns features">
-        <div class="column is-12">
-          <div class="card is-shady">
-            <div class="card-content">
-              <div class="content">
-                <nuxt-link to="/miso/miso-karuta">
-                  <h4>ミソカルタ</h4>
-                </nuxt-link>
-                <p>ポエムコアの好きなパンチライン</p>
-              </div>
-            </div>
-          </div>
-          <div class="card is-shady">
-            <div class="card-content">
-              <div class="content">
-                <nuxt-link to="/miso/miso-karuta-gen">
-                  <h4>ミソカルタメーカー</h4>
-                </nuxt-link>
-                <p>パンチラインをみんなで共有</p>
-              </div>
-            </div>
-          </div>
-          <div class="card is-shady">
-            <div class="card-content">
-              <div class="content">
-                <nuxt-link to="/likes/">
-                  <h4>これ好き</h4>
-                </nuxt-link>
-                <p>好きなものを好きといっているだけのコンテンツ</p>
-              </div>
-            </div>
+  <div class="columns features is-desktop">
+    <div class="column is-6">
+      <div class="card is-shady">
+        <div class="card-content">
+          <div class="content">
+            <nuxt-link to="/miso/miso-karuta">
+              <h4>ミソカルタ</h4>
+            </nuxt-link>
+            <p>ポエムコアのパンチラインまとめ</p>
           </div>
         </div>
       </div>
-    </section>
-  </section>
+      <div class="card is-shady">
+        <div class="card-content">
+          <div class="content">
+            <nuxt-link to="/miso/miso-karuta-gen">
+              <h4>ミソカルタメーカー</h4>
+            </nuxt-link>
+            <p>パンチラインをみんなで共有できる</p>
+          </div>
+        </div>
+      </div>
+      <div class="card is-shady">
+        <div class="card-content">
+          <div class="content">
+            <nuxt-link to="/likes/">
+              <h4>これ好き</h4>
+            </nuxt-link>
+            <p>好きなものについてかいてる</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
 </script>
 <style lang="scss">
 .card {
-  margin: 10px;
+  margin: 0.3rem;
 }
 </style>
